08549fa6fba45cbb020c5fec712f64039725c1c7
= <=>
2025-09-04 14:03:19 +0500

    added

---
+++ app.js
@@ -0,0 +1,17 @@
+// Q1
+// Answer
+
+// const datetime = new Date("Saturday, 9 August 2025")
+// function formateddate(){
+// return   ("datetime" , JSON.stringify("Saturday, 9 August 2025"))
+// }
+// console.log(formateddate())
+
+
+// Q2
+// Answer
+let satartTime = new Date();
+let andtime = new Date();
+console.log(andtime)
+
+
